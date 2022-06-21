d24540c5b565eea0c2ede8fb42e643d5acaf4856
Kirill Grunin <jettyk@gmail.com>
2022-06-21 21:46:04 +0500

    Initial commit

---
+++ ToDoList/TableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TableViewController.swift
+//  ToDoList
+//
+//  Created by Kirill Grunin on 21.06.2022.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var tasks: [String] = []
+    
+    @IBAction func saveTask(_ sender: UIBarButtonItem) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tasks.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = tasks[indexPath.row]
+
+        return cell
+    }
+}
